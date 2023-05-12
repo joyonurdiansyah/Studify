3e1c31bbdd8bc9f3f54ddc6c205a140f42353202
Joyo Nurdiansyah <nurdiansyahjoyo@gmail.com>
2023-05-12 08:33:59 +0700

    revisi manajemen file

--- index.php
+++ index.php
@@ -1,21 +1,23 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="./css/style.index.css">
-    <title>Home</title>
-</head>
+
+<!-- ini header.php -->
+<?php
+  include './html/include/header.php';
+?>
+
 <body>
-<body>
-	<h1>Selamat datang di Web Mahasiswa</h1>
-	<p>Silahkan pilih menu:</p>
-	<ul>
-		<li><a href="html/data_mahasiswa.php">Data Mahasiswa</a></li>
-		<li><a href="html/add_mahasiswa.php">Tambah Mahasiswa</a></li>
-		<li><a href="html/api_mahasiswa.php">API Mahasiswa</a></li>
-	</ul>
-</body>
+
+<!-- ini menu.php -->
+<?php
+  include './html/include/menu.php';
+?>
+
+<div>
+  <p>ini halaman table</p>
+</div>
+<!-- ini footer.php -->
+<?php
+  include './html/include/footer.php';
+?>
+
 </body>
 </html>
